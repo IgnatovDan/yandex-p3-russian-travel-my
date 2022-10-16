36d01858b6cb606d307dbae571b67bfbc2a578c6
Ignatov Dan <ignatovdan@gmail.com>
2022-10-17 01:08:17 +0300

    Try avoid different font paint in win vs linux

--- tests-testcafe/1280/index-tests_1280.js
+++ tests-testcafe/1280/index-tests_1280.js
@@ -5,5 +5,11 @@ fixture`Index page screenshot 1280`
 
 test(`test1`, async t => {
   await t.resizeWindow(1280, 800);
-  await t.expect(await compareScreenshot(t, 'index-page_1280.png')).ok();
+  const screenshotComparerDefault = {
+    looksSameComparisonOptions: {
+      tolerance: 8,
+    }
+  };
+
+  await t.expect(await compareScreenshot(t, 'index-page_1280.png', null, screenshotComparerDefault)).ok();
 });
