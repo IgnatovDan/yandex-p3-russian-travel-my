9b6ec49cf0af187ee0a7677ec4da8a4771d6eb19
Ignatov Dan <ignatovdan@gmail.com>
2022-10-19 20:19:28 +0300

    Check sections for 320px

--- tests-testcafe/320/index-tests_320.js
+++ tests-testcafe/320/index-tests_320.js
@@ -1,4 +1,5 @@
 import { compareScreenshot } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
 
 fixture`Index page screenshot 320`
   .page('../../index.html');
@@ -6,4 +7,22 @@ fixture`Index page screenshot 320`
 test(`Index page screenshot test 320`, async t => {
   await t.resizeWindow(320, 1000);
   await t.expect(await compareScreenshot(t, 'index-page_320.png')).ok();
+});
+
+test(`Header screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.header');
+  await t.expect(await compareScreenshot(t, 'header_320.png', element)).ok();
+});
+
+test(`Lead screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.lead');
+  await t.expect(await compareScreenshot(t, 'lead_320.png', element)).ok();
+});
+
+test(`Intro screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.intro');
+  await t.expect(await compareScreenshot(t, 'intro_320.png', element)).ok();
 });
