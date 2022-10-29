2e78b3e1471f8d463cbf3ca039eabe3171245b91
Ignatov Dan <ignatovdan@gmail.com>
2022-10-29 23:59:15 +0300

    Sync etalons, use 'section-pages'

--- tests-testcafe/320/index-tests_320.js
+++ tests-testcafe/320/index-tests_320.js
@@ -9,14 +9,32 @@ test(`Index page screenshot test 320`, async t => {
   await t.expect(await compareScreenshot(t, 'index-page_320.png')).ok();
 });
 
-test(`Header screenshot test 320`, async t => {
+test.page`../section-pages/header.html`(`Header screenshot test 320`, async t => {
   await t.resizeWindow(320, 1000);
   const element = Selector('.header');
   await t.expect(await compareScreenshot(t, 'header_320.png', element)).ok();
 });
 
-test(`Lead screenshot test 320`, async t => {
+test.page`../section-pages/lead.html`(`Lead screenshot test 320`, async t => {
   await t.resizeWindow(320, 1000);
   const element = Selector('.lead');
   await t.expect(await compareScreenshot(t, 'lead_320.png', element)).ok();
+});
+
+test.page`../section-pages/intro.html`(`Intro screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.intro');
+  await t.expect(await compareScreenshot(t, 'intro_320.png', element)).ok();
+});
+
+test.page`../section-pages/photo-grid.html`(`Photo-grid screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.photo-grid');
+  await t.expect(await compareScreenshot(t, 'photo-grid_320.png', element)).ok();
+});
+
+test.page`../section-pages/places.html`(`Places screenshot test 320`, async t => {
+  await t.resizeWindow(320, 1000);
+  const element = Selector('.places');
+  await t.expect(await compareScreenshot(t, 'places_320.png', element)).ok();
 });
