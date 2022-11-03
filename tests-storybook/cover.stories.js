06debf5199e1c8adf988b14459a225b4212c5c2b
Ignatov Dan <ignatovdan@gmail.com>
2022-11-03 17:54:02 +0300

    Add createStorybookConfigByWidth

--- tests-storybook/cover.stories.js
+++ tests-storybook/cover.stories.js
@@ -1,5 +1,7 @@
+import { createStorybookConfigByWidth } from './utils/utils.js';
+  
 export default {
-  title: 'Header',
+  title: 'Cover',
 }
 
 const Template = () => {
@@ -19,83 +21,13 @@ const Template = () => {
   return result;
 };
 
-export const Width_320 = Object.assign(
-  // TODO: add a new function in utils.js, return all resolutions in a single object, export each member as a named variable
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '320' },
-      chromatic: { viewports: [320] },
-    }
-  }
-);
+const storybookConfig = createStorybookConfigByWidth(Template);
 
-export const Width_480 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '480' },
-      chromatic: { viewports: [480] },
-    }
-  }
-);
-
-export const Width_768 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '768' },
-      chromatic: { viewports: [768] },
-    }
-  }
-);
-
-export const Width_900 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '900' },
-      chromatic: { viewports: [900] },
-    }
-  }
-);
-
-export const Width_1024 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '1024' },
-      chromatic: { viewports: [1024] },
-    }
-  }
-);
-
-export const Width_1080 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '1080' },
-      chromatic: { viewports: [1080] },
-    }
-  }
-);
-
-export const Width_1280 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '1280' },
-      chromatic: { viewports: [1280] },
-    }
-  }
-);
-
-export const Width_1440 = Object.assign(
-  Template.bind({}),
-  {
-    parameters: {
-      viewport: { defaultViewport: '1440' },
-      chromatic: { viewports: [1440] },
-    }
-  }
-);
+export const Width_320 = storybookConfig.Width_320;
+export const Width_480 = storybookConfig.Width_480;
+export const Width_768 = storybookConfig.Width_768;
+export const Width_900 = storybookConfig.Width_900;
+export const Width_1024 = storybookConfig.Width_1024;
+export const Width_1080 = storybookConfig.Width_1080;
+export const Width_1280 = storybookConfig.Width_1280;
+export const Width_1440 = storybookConfig.Width_1440;
