4ed35c2932f4c9402d1fcc74a07e2499c55e6e31
Ignatov Dan <ignatovdan@gmail.com>
2022-11-08 13:30:09 +0300

    Process todos (#26)
    * Process cover and lead todos
    
    * Process todos in readme/lead/places
    
    * Add 'local' to script name

--- tests-storybook/lead.stories.js
+++ tests-storybook/lead.stories.js
@@ -14,15 +14,12 @@ const Template = () => {
           <h1 class="lead__title">Путешествия по&nbsp;России</h1>
           <p class="lead__subtitle">Настоящая страна не в выпусках новостей, а здесь.</p>
 
-          <!-- TODO: calc(100vw - 148px * 2) - calc(1280px - 148px * 2) -->
           <img class="lead__image" src="./images/lead-image-320.jpg" alt="карта с маршрутом" srcset="
           ./images/lead-image-320.jpg 320w, 
           ./images/lead-image-768.jpg 768w,
           ./images/lead-image-928.jpg 928w,
           ./images/lead-image-984.jpg 984w"
-            sizes="(max-width: 1024px) 100vw, (max-width: 1280px) calc(100vw - 48px * 2), (min-width: 1280px) calc(100vw - 148px * 2)">
-          <!-- TODO: sync position for 1024/1280 -->
-          <!-- TODO: sync font for 768 -->
+            sizes="(max-width: 1280px) 100vw, (min-width: 1280px) calc(1280px - 148px * 2)">
           <p class="lead__caption">ваша полка — верхняя</p>
         </section>
       </main>
